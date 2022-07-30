831be1a9faef23af317f716d031925bbbedc0b31
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-07-30 13:12:37 +0530

    added faq page

---
+++ pages/faq.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Layout,FAQComp} from "../src/components"
+
+const FAQ = () => {
+  return (
+    <Layout title={"FAQ"} description={""}>
+        <FAQComp />
+    </Layout>
+  )
+}
+
+export default FAQ
---
+++ src/components/Normal/FAQ.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
+import { motion, AnimatePresence } from "framer-motion"
+
+import { questions } from '../../../utils/data'
+
+const FAQComp = () => {
+    const [showAns, setShowAns] = useState({ id: null, show: false })
+    const [showQues, setShowQues] = useState("owner")
+    return (
+        <div className="rokye__faq">
+            <div className="rokye__faq-title">
+                <h1>Frequently Asked Questions</h1>
+            </div>
+            <div className="rokye__faq-tab">
+                <div className={`item ${showQues==="owner"&& "active"}`} onClick={()=>setShowQues("owner")}>
+                    <h3>For Owner</h3>
+                </div>
+                <div className={`item ${showQues==="tenant"&& "active"}`} onClick={()=>setShowQues("tenant")}>
+                    <h3>For Tenant</h3>
+                </div>
+                <div className={`item ${showQues==="referral"&& "active"}`} onClick={()=>setShowQues("referral")}>
+                    <h3>For Referrals</h3>
+                </div>
+            </div>
+            <div className="rokye__faq-content">
+
+                {
+                    questions[showQues].map((item) => (
+
+                        <div className="item" key={item.id}>
+                            <div className="item__upper" onClick={() => setShowAns({ id: showAns.id === item.id ? null : item.id, show: showAns.id === item.id ? false : true })}>
+                                <div className="quest">
+                                    <div className="circle" />
+                                    <h2>{item.quest}</h2>
+                                </div>
+                                <div className="show">
+                                    {
+                                        (showAns.id === item.id && showAns.show) ? (
+                                            <AiOutlineMinus size={25} />
+                                        ) : (
+                                            <AiOutlinePlus size={25} />
+                                        )
+                                    }
+                                </div>
+                            </div>
+                            <div className="divider" />
+                            <AnimatePresence>
+
+
+                                <div className={`item__ans ${showAns.id === item.id && showAns.show ? "active" : ""}`} >
+                                    <p>{item.ans} <br /> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus ratione recusandae ipsa ex nemo optio ut accusamus? Nobis accusamus consequatur quia voluptate minus molestiae repudiandae, tempore nam culpa? Fuga, illo!</p>
+                                </div>
+
+                            </AnimatePresence>
+                        </div>
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default FAQComp
