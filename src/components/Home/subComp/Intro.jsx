c42ecc5616323e615b477d9263637fb5d317e7c1
Jaiswar <jaiswarnilesh2002@gmail.com>
2022-08-01 20:24:19 +0530

    updated

--- src/components/Home/subComp/Intro.jsx
+++ src/components/Home/subComp/Intro.jsx
@@ -1,31 +1,15 @@
 import React from 'react'
-import Image from 'next/image'
-import { motion } from "framer-motion"
-import { VscHome } from "react-icons/vsc"
-import { MdOutlineAutoAwesome } from "react-icons/md"
 
-const Intro = ({ winWidth }) => {
-    const url = "https://o.remove.bg/downloads/5489a143-a2ee-4dae-b5eb-8e9bae0f065e/photo-1479839672679-a46483c0e7c8-removebg-preview.png"
-    const isResponsiveFrame=winWidth > 1000 || winWidth < 540
+
+const Intro = () => {
     return (
         <div className="rokye__home-intro">
-            <div className="text">
-                <h1>Find</h1>
-                <h1 className='mid'>Your <MdOutlineAutoAwesome size={50} color="#F2BA52" /> </h1>
-                <h1>
-                    &nbsp;<VscHome color='#F2BA52' size={100} /> Home
-                </h1>
+            <div className="blob1" />
+            <div className="content">
+                <h1>New way of renting a home</h1>
+                <h3>No brokerage, No advance payment and all the listings are physically verified.</h3>
             </div>
-            {
-                isResponsiveFrame  && (
-                    <div className="img">
-                        <Image src="/blob.png" alt="" width={600} height={600} />
-                        <div className="img__inner">
-                            <img src={"/banner.png"} alt="" width={600} />
-                        </div>
-                    </div>
-                )
-            }
+            <div className="blob2" />
         </div>
     )
 }
